adf0b30646a8bd6f4448bca4f14b2dbd0c69955b
Johannes Binder <j.binder.x@gmail.com>
2013-04-08 21:14:03 +0200

    basic usage of distorm, printing the output to the console only for now

--- disassemble.py
+++ disassemble.py
@@ -19,11 +19,20 @@ from dff.api.module.module import Module
 from dff.api.module.script import Script
 from dff.api.types.libtypes import Argument, typeId
 
+from distorm3 import Decode, Decode16Bits, Decode32Bits, Decode64Bits
+
 class Disassemble(Script):
   def __init__(self):
     Script.__init__(self, "disassemble")
 
   def start(self, args):
+    self.stateinfo = "started..."
+    node = args['file'].value()
+    vfile = node.open()
+    disassembly = Decode(0x0, vfile.read(), Decode32Bits) # TODO: determine correct decoder flag
+    for i in disassembly:
+      print "0x%08x (%02x) %-20s %s" % (i[0],  i[1],  i[3],  i[2])
+    vfile.close()
     pass
 
 class disassemble(Module):
